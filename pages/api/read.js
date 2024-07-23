fe465a819b1655faa6c0865c5040d251310358ed
prashantSj789 <singhrashant79072@gmail.com>
2024-07-23 08:35:21 +0530

    skfls

--- pages/api/read.js
+++ pages/api/read.js
@@ -5,6 +5,6 @@ export default function handler(req, res) {
   if (req.method === 'GET') {
     res.status(200).json(data);
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: 'Method not allowed!!' });
   }
 }
