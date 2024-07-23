e50ba4406883563c0315a0872f27e2e45e1c66fd
prashantSj789 <singhrashant79072@gmail.com>
2024-07-23 08:59:24 +0530

    using pm2 now

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "inventory_test",
+            script: "node_modules/next/dist/bin/next",
+            args: "start -p 3333", //running on port 3000
+            watch: false,
+        },
+    ],
+};
